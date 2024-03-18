e9ee2fd2ba1682745453bad2c58e5e288cd48e31
RohanQ17 <axelb4467@gmail.com>
2024-03-18 14:23:02 +0530

    rendered photos and prices , total prices of orders in checkout etc

--- pika/models.py
+++ pika/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100, null=True)
     email = models.CharField(max_length=100, null=True)
     def __str__(self):
@@ -12,9 +12,16 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     digital = models.BooleanField(default=False, null=True, blank=True)
-    #image
+    image = models.ImageField(null = True, blank = True)
     def __str__(self):
         return self.name
+    @property
+    def image_url(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,null=True, blank=True)  # foreign key means one to many relationship that means that one user can have multiple orders
     date_order = models.DateTimeField(auto_now_add=True)
@@ -23,12 +30,25 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
-
+    @property
+    def cart_price(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    @property
+    def cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL,null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL,null=True, blank=True)
     quantity = models.IntegerField(default=0,null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
     def __str__(self):
         return self.product.name
--- pika/views.py
+++ pika/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-
+from .models import*
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+
+    context = {'products': products}
     return render(request, 'store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,completed=False)
+        items = order.orderitem_set.all()   # parent ko pehle call then . child then some attribute like _set
+    else:
+        items = []
+        order = {'cart_price': 0, 'cart_items': 0}
+    context = {'items': items, 'order': order}
     return render(request, 'cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, completed=False)
+        items = order.orderitem_set.all()  # parent ko pehle call then . child then some attribute like _set
+    else:
+        items = []
+        order = {'cart_price': 0, 'cart_items': 0}
+    context = {'items': items, 'order': order}
+
     return render(request, 'checkout.html', context)
